f011f5b340b924edab515d298aea00aaa9f912b1
Brian Zhang <zhang.15977@osu.edu>
2026-03-03 11:48:53 -0500

    Create outline for ship class & properties
    Implement hits method
    
    Implement isSunk method

---
+++ src/ship.js
@@ -0,0 +1,27 @@
+/**
+ * Ship object - creates a ship object which acts like a ship in battleship
+ */
+export default function Ship(shipLength) {
+    // object properties
+    let length = shipLength;
+    let hits = 0;
+    let sunk = false;
+
+    // methods
+    
+    /**
+     * Increaes the amount of times the ship has been hit
+     */
+    const hit = () => { 
+        hits++;
+    }
+
+    /**
+     * Returns if the ship has been sunk
+     */
+    const isSunk = () => { 
+        length - hits  > 0 ? sunk = true : sunk = false;
+    }
+
+    return {hit, isSunk}
+}
