987de26aa3080f125e2df1eb2543f8a83418ce6c
Brian Zhang <zhang.15977@osu.edu>
2026-03-03 11:48:53 -0500

    Create outline for ship class & properties

---
+++ src/ship.js
@@ -0,0 +1,23 @@
+/**
+ * Ship object - creates a ship object which acts like a ship in battleship
+ */
+export default function Ship(shipLength) {
+    // object properties
+    let length = shipLength;
+    let hits = 0;
+    let sunk = 0;
+
+    // methods
+    
+    /**
+     * Increaes the amount of times the ship has been hit
+     */
+    const hit = () => { }
+
+    /**
+     * Returns if the ship has been sunk
+     */
+    const isSunk = () => { }
+
+    return {hit, isSunk}
+}
