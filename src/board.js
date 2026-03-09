651c96067ee767a446fa01aeaa2c37f17e51427b
Brian Zhang <zhang.15977@osu.edu>
2026-03-09 15:41:21 -0400

    Create outline of board class implementation
    add length parameter to placeShip()
    
    implement placeShip
    
    implement recieveAttack
    
    update allSunk check in reciveAttack
    
    typo
    
    debugg recieveAttack to correctly check if allSunk
    
    added getters to object, to properly get object properties
    
    debug getters & make sure board is properly initialized
    
    debug placeShip to properly place in row/col
    
    typo
    
    debug placeShip logic to be correct

---
+++ src/board.js
@@ -0,0 +1,80 @@
+import Ship from "./ship";
+
+/**
+ * This is a board object, which stores the postition of ships and misses
+ */
+export default function Board(size) { 
+    // board states
+    const CellState = Object.freeze({
+        EMPTY: 0,
+        SHIP: 1,
+        HIT: 2,
+        MISS: 3,
+    });
+    
+
+    // Properties:
+
+    /** 
+     * board - 2d board storing the state of each cell, as well as a refrence to a ship class.
+     * allSunk - true if all ships are sunk, else false.
+     */
+    const board = Array.from({ length: size }, () =>
+        Array.from({ length: size }, () => ({
+            state: CellState.EMPTY,
+            ship: null
+    })));
+    let allSunk = true;
+
+    // Methods
+
+    // getters 
+    const sunk = () => allSunk
+
+    /**
+     * Places an instance of a ship on the board.
+     *  x - x cooardinate of ship (starting pos.)
+     *  y - y cooardinate of ship (starting pos.)
+     *  length - length of ship
+     *  isHorizontal - if the ship is being placed horizontally or vertically.
+     */
+    const placeShip = (x, y, length, isHorizontal) => {
+        const ship = Ship(length);
+        allSunk = false;
+
+        if (isHorizontal) {
+            for (let i = x; i < x + length; i++) {
+                board[i][y].state =  CellState.SHIP;
+                board[i][y].ship = ship;
+            }
+        } else {
+            for (let j = y; j < y + length; j++) {
+                board[x][j].state = CellState.SHIP;
+                board[x][j].ship = ship;
+            }
+        }
+    }
+
+    /**
+     * Takes in coordinates of an attack, then determines if the attack hit or missed.
+     *  x - x cooardinate of attack
+     *  y - y cooardinate of attack
+     */
+    const recieveAttack = (x, y) => {
+       switch (board[x][y].state) {
+        case CellState.EMPTY:
+            board[x][y].state = CellState.MISS;
+            break;
+        case CellState.SHIP:
+            board[x][y].state = CellState.HIT;
+            board[x][y].ship.hit();
+            break;
+        default:
+            break;
+        }
+
+       allSunk = !board.flat().some(cell => cell.state === CellState.SHIP);
+    }
+
+    return {board, sunk, placeShip, recieveAttack}
+}
